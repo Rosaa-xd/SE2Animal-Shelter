b48db35b29b79c579569800325817e0e4747aefc
Roos <mm.heijkoop@student.fontys.nl>
2016-03-16 21:13:27 +0100

    AnimalShelter Unit Test added

---
+++ AnimalShelter.Test/TestCat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnimalShelter.Test
+{
+    [TestClass]
+    public class TestCat
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+
+
+        }
+    }
+}
