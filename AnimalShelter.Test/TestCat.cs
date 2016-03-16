6b5235faebb7b58ecc5b388e7296374c748f8eef
Rosa <git@shadowphoenix.nl>
2016-03-16 22:39:48 +0100

    Added Unit tests for Cat and Dog

--- AnimalShelter.Test/TestCat.cs
+++ AnimalShelter.Test/TestCat.cs
@@ -7,17 +7,37 @@ namespace AnimalShelter.Test
     public class TestCat
     {
         [TestMethod]
-        public void TestConstructor()
+        public void CatTestConstructor()
         {
             // Arrange
-
+            Cat cat = new Cat("Gabby", "15", Gender.Female, null, null);
 
             // Act
-
+            string catName = cat.Name;
+            string catAge = cat.Age;
+            Gender catGender = cat.Gender;
+            string catIsReserved = cat.IsReserved;
+            string catBadHabit = cat.BadHabit;
 
             // Assert
+            Assert.AreEqual("Gabby", catName);
+            Assert.AreEqual("15", catAge);
+            Assert.AreEqual(Gender.Female, catGender);
+            Assert.AreEqual(null, catIsReserved);
+            Assert.AreEqual(null, catBadHabit);
+        }
 
+        [TestMethod]
+        public void CatTestToString()
+        {
+            // Arrange
+            Cat cat = new Cat("Harry", "17", Gender.Male, null, "Stinks");
 
+            // Act
+            string catToString = cat.ToString();
+
+            // Assert
+            Assert.AreEqual("Harry 17 Male Stinks", catToString);
         }
     }
 }
---
+++ AnimalShelter.Test/TestDog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnimalShelter.Test
+{
+    [TestClass]
+    public class TestDog
+    {
+        [TestMethod]
+        public void DogTestConstructor()
+        {
+            // Arrange
+            Dog dog = new Dog("Bobby", "4", Gender.Male, null, new DateTime(2016, 3, 16));
+            
+            // Act
+            string dogName = dog.Name;
+            string dogAge = dog.Age;
+            Gender dogGender = dog.Gender;
+            string dogIsReserved = dog.IsReserved;
+            DateTime dogLastWalkDay = dog.LastWalkDay;
+
+            // Assert
+            Assert.AreEqual("Bobby", dogName);
+            Assert.AreEqual("4", dogAge);
+            Assert.AreEqual(Gender.Male, dogGender);
+            Assert.AreEqual(null, dogIsReserved);
+            Assert.AreEqual(new DateTime(2016, 3, 16), dogLastWalkDay);
+        }
+
+        [TestMethod]
+        public void DogTestToString()
+        {
+            // Arrange
+            Dog dog = new Dog("Bobby", "4", Gender.Male, null, new DateTime(2016, 3, 16));
+
+            // Act
+            string dogToString = dog.ToString();
+
+            // Assert
+            Assert.AreEqual("Bobby 4 Male 16-03-2016", dogToString);           
+        }
+    }
+}
