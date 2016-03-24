8e42130c2854ed01f5a1f01263b4ac758f95a87f
Rosaa-xD <github@shadowphoenix.nl>
2016-03-25 00:26:00 +0100

    Completed WebShopForm interface

--- AnimalShelter/WebShopForm.Designer.cs
+++ AnimalShelter/WebShopForm.Designer.cs
@@ -29,29 +29,31 @@
         private void InitializeComponent()
         {
             this.gb_animals = new System.Windows.Forms.GroupBox();
-            this.lb_Animals = new System.Windows.Forms.ListBox();
-            this.lbl_ReservorName = new System.Windows.Forms.Label();
-            this.tb_ReservorName = new System.Windows.Forms.TextBox();
-            this.cb_AnimalBuyers = new System.Windows.Forms.ComboBox();
-            this.lbl_AnimalPrice = new System.Windows.Forms.Label();
+            this.btn_ReturnToAdminForm = new System.Windows.Forms.Button();
+            this.btn_ReserveAnimal = new System.Windows.Forms.Button();
+            this.btn_BuyAnimal = new System.Windows.Forms.Button();
             this.lbl_ShowAnimalPrice = new System.Windows.Forms.Label();
+            this.lbl_AnimalPrice = new System.Windows.Forms.Label();
+            this.cb_AnimalBuyers = new System.Windows.Forms.ComboBox();
+            this.tb_ReservorName = new System.Windows.Forms.TextBox();
+            this.lbl_ReservorName = new System.Windows.Forms.Label();
+            this.lb_Animals = new System.Windows.Forms.ListBox();
             this.gb_Products = new System.Windows.Forms.GroupBox();
+            this.btn_AddProduct = new System.Windows.Forms.Button();
+            this.btn_BuyProduct = new System.Windows.Forms.Button();
             this.lbl_ShowProductPrice = new System.Windows.Forms.Label();
             this.lbl_ProductPrice = new System.Windows.Forms.Label();
             this.cb_ProductBuyers = new System.Windows.Forms.ComboBox();
             this.tb_BuyerName = new System.Windows.Forms.TextBox();
             this.lbl_BuyerName = new System.Windows.Forms.Label();
             this.lb_Products = new System.Windows.Forms.ListBox();
-            this.btn_BuyAnimal = new System.Windows.Forms.Button();
-            this.btn_BuyProduct = new System.Windows.Forms.Button();
-            this.btn_AddProduct = new System.Windows.Forms.Button();
-            this.btn_ReserveAnimal = new System.Windows.Forms.Button();
             this.gb_animals.SuspendLayout();
             this.gb_Products.SuspendLayout();
             this.SuspendLayout();
             // 
             // gb_animals
             // 
+            this.gb_animals.Controls.Add(this.btn_ReturnToAdminForm);
             this.gb_animals.Controls.Add(this.btn_ReserveAnimal);
             this.gb_animals.Controls.Add(this.btn_BuyAnimal);
             this.gb_animals.Controls.Add(this.lbl_ShowAnimalPrice);
@@ -62,18 +64,75 @@
             this.gb_animals.Controls.Add(this.lb_Animals);
             this.gb_animals.Location = new System.Drawing.Point(13, 13);
             this.gb_animals.Name = "gb_animals";
-            this.gb_animals.Size = new System.Drawing.Size(308, 288);
+            this.gb_animals.Size = new System.Drawing.Size(308, 244);
             this.gb_animals.TabIndex = 0;
             this.gb_animals.TabStop = false;
             this.gb_animals.Text = "Animals";
             // 
-            // lb_Animals
+            // btn_ReturnToAdminForm
             // 
-            this.lb_Animals.FormattingEnabled = true;
-            this.lb_Animals.Location = new System.Drawing.Point(159, 19);
-            this.lb_Animals.Name = "lb_Animals";
-            this.lb_Animals.Size = new System.Drawing.Size(143, 264);
-            this.lb_Animals.TabIndex = 0;
+            this.btn_ReturnToAdminForm.Location = new System.Drawing.Point(10, 196);
+            this.btn_ReturnToAdminForm.Name = "btn_ReturnToAdminForm";
+            this.btn_ReturnToAdminForm.Size = new System.Drawing.Size(67, 39);
+            this.btn_ReturnToAdminForm.TabIndex = 8;
+            this.btn_ReturnToAdminForm.Text = "Return";
+            this.btn_ReturnToAdminForm.UseVisualStyleBackColor = true;
+            this.btn_ReturnToAdminForm.Click += new System.EventHandler(this.btn_ReturnToAdminForm_Click);
+            // 
+            // btn_ReserveAnimal
+            // 
+            this.btn_ReserveAnimal.Location = new System.Drawing.Point(86, 151);
+            this.btn_ReserveAnimal.Name = "btn_ReserveAnimal";
+            this.btn_ReserveAnimal.Size = new System.Drawing.Size(67, 39);
+            this.btn_ReserveAnimal.TabIndex = 7;
+            this.btn_ReserveAnimal.Text = "Reserve";
+            this.btn_ReserveAnimal.UseVisualStyleBackColor = true;
+            this.btn_ReserveAnimal.Click += new System.EventHandler(this.btn_ReserveAnimal_Click);
+            // 
+            // btn_BuyAnimal
+            // 
+            this.btn_BuyAnimal.Location = new System.Drawing.Point(86, 196);
+            this.btn_BuyAnimal.Name = "btn_BuyAnimal";
+            this.btn_BuyAnimal.Size = new System.Drawing.Size(67, 39);
+            this.btn_BuyAnimal.TabIndex = 6;
+            this.btn_BuyAnimal.Text = "Buy";
+            this.btn_BuyAnimal.UseVisualStyleBackColor = true;
+            this.btn_BuyAnimal.Click += new System.EventHandler(this.btn_BuyAnimal_Click);
+            // 
+            // lbl_ShowAnimalPrice
+            // 
+            this.lbl_ShowAnimalPrice.AutoSize = true;
+            this.lbl_ShowAnimalPrice.Location = new System.Drawing.Point(47, 90);
+            this.lbl_ShowAnimalPrice.Name = "lbl_ShowAnimalPrice";
+            this.lbl_ShowAnimalPrice.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lbl_ShowAnimalPrice.Size = new System.Drawing.Size(12, 13);
+            this.lbl_ShowAnimalPrice.TabIndex = 5;
+            this.lbl_ShowAnimalPrice.Text = "x";
+            // 
+            // lbl_AnimalPrice
+            // 
+            this.lbl_AnimalPrice.AutoSize = true;
+            this.lbl_AnimalPrice.Location = new System.Drawing.Point(7, 90);
+            this.lbl_AnimalPrice.Name = "lbl_AnimalPrice";
+            this.lbl_AnimalPrice.Size = new System.Drawing.Size(34, 13);
+            this.lbl_AnimalPrice.TabIndex = 4;
+            this.lbl_AnimalPrice.Text = "Price:";
+            // 
+            // cb_AnimalBuyers
+            // 
+            this.cb_AnimalBuyers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_AnimalBuyers.FormattingEnabled = true;
+            this.cb_AnimalBuyers.Location = new System.Drawing.Point(10, 63);
+            this.cb_AnimalBuyers.Name = "cb_AnimalBuyers";
+            this.cb_AnimalBuyers.Size = new System.Drawing.Size(143, 21);
+            this.cb_AnimalBuyers.TabIndex = 3;
+            // 
+            // tb_ReservorName
+            // 
+            this.tb_ReservorName.Location = new System.Drawing.Point(10, 36);
+            this.tb_ReservorName.Name = "tb_ReservorName";
+            this.tb_ReservorName.Size = new System.Drawing.Size(143, 20);
+            this.tb_ReservorName.TabIndex = 2;
             // 
             // lbl_ReservorName
             // 
@@ -84,39 +143,14 @@
             this.lbl_ReservorName.TabIndex = 1;
             this.lbl_ReservorName.Text = "Name";
             // 
-            // tb_ReservorName
+            // lb_Animals
             // 
-            this.tb_ReservorName.Location = new System.Drawing.Point(10, 36);
-            this.tb_ReservorName.Name = "tb_ReservorName";
-            this.tb_ReservorName.Size = new System.Drawing.Size(143, 20);
-            this.tb_ReservorName.TabIndex = 2;
-            // 
-            // cb_AnimalBuyers
-            // 
-            this.cb_AnimalBuyers.FormattingEnabled = true;
-            this.cb_AnimalBuyers.Location = new System.Drawing.Point(10, 63);
-            this.cb_AnimalBuyers.Name = "cb_AnimalBuyers";
-            this.cb_AnimalBuyers.Size = new System.Drawing.Size(143, 21);
-            this.cb_AnimalBuyers.TabIndex = 3;
-            // 
-            // lbl_AnimalPrice
-            // 
-            this.lbl_AnimalPrice.AutoSize = true;
-            this.lbl_AnimalPrice.Location = new System.Drawing.Point(7, 91);
-            this.lbl_AnimalPrice.Name = "lbl_AnimalPrice";
-            this.lbl_AnimalPrice.Size = new System.Drawing.Size(34, 13);
-            this.lbl_AnimalPrice.TabIndex = 4;
-            this.lbl_AnimalPrice.Text = "Price:";
-            // 
-            // lbl_ShowAnimalPrice
-            // 
-            this.lbl_ShowAnimalPrice.AutoSize = true;
-            this.lbl_ShowAnimalPrice.Location = new System.Drawing.Point(140, 91);
-            this.lbl_ShowAnimalPrice.Name = "lbl_ShowAnimalPrice";
-            this.lbl_ShowAnimalPrice.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.lbl_ShowAnimalPrice.Size = new System.Drawing.Size(12, 13);
-            this.lbl_ShowAnimalPrice.TabIndex = 5;
-            this.lbl_ShowAnimalPrice.Text = "x";
+            this.lb_Animals.FormattingEnabled = true;
+            this.lb_Animals.Location = new System.Drawing.Point(159, 19);
+            this.lb_Animals.Name = "lb_Animals";
+            this.lb_Animals.Size = new System.Drawing.Size(143, 212);
+            this.lb_Animals.TabIndex = 0;
+            this.lb_Animals.SelectedIndexChanged += new System.EventHandler(this.lb_Animals_SelectedIndexChanged);
             // 
             // gb_Products
             // 
@@ -128,19 +162,39 @@
             this.gb_Products.Controls.Add(this.tb_BuyerName);
             this.gb_Products.Controls.Add(this.lbl_BuyerName);
             this.gb_Products.Controls.Add(this.lb_Products);
-            this.gb_Products.Location = new System.Drawing.Point(330, 13);
+            this.gb_Products.Location = new System.Drawing.Point(330, 12);
             this.gb_Products.Name = "gb_Products";
-            this.gb_Products.Size = new System.Drawing.Size(308, 288);
+            this.gb_Products.Size = new System.Drawing.Size(308, 245);
             this.gb_Products.TabIndex = 1;
             this.gb_Products.TabStop = false;
             this.gb_Products.Text = "Products";
             // 
+            // btn_AddProduct
+            // 
+            this.btn_AddProduct.Location = new System.Drawing.Point(10, 197);
+            this.btn_AddProduct.Name = "btn_AddProduct";
+            this.btn_AddProduct.Size = new System.Drawing.Size(67, 39);
+            this.btn_AddProduct.TabIndex = 8;
+            this.btn_AddProduct.Text = "Add";
+            this.btn_AddProduct.UseVisualStyleBackColor = true;
+            this.btn_AddProduct.Click += new System.EventHandler(this.btn_AddProduct_Click);
+            // 
+            // btn_BuyProduct
+            // 
+            this.btn_BuyProduct.Location = new System.Drawing.Point(86, 197);
+            this.btn_BuyProduct.Name = "btn_BuyProduct";
+            this.btn_BuyProduct.Size = new System.Drawing.Size(67, 39);
+            this.btn_BuyProduct.TabIndex = 7;
+            this.btn_BuyProduct.Text = "Buy";
+            this.btn_BuyProduct.UseVisualStyleBackColor = true;
+            this.btn_BuyProduct.Click += new System.EventHandler(this.btn_BuyProduct_Click);
+            // 
             // lbl_ShowProductPrice
             // 
             this.lbl_ShowProductPrice.AutoSize = true;
-            this.lbl_ShowProductPrice.Location = new System.Drawing.Point(140, 91);
+            this.lbl_ShowProductPrice.Location = new System.Drawing.Point(47, 92);
             this.lbl_ShowProductPrice.Name = "lbl_ShowProductPrice";
-            this.lbl_ShowProductPrice.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbl_ShowProductPrice.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.lbl_ShowProductPrice.Size = new System.Drawing.Size(12, 13);
             this.lbl_ShowProductPrice.TabIndex = 5;
             this.lbl_ShowProductPrice.Text = "x";
@@ -156,6 +210,7 @@
             // 
             // cb_ProductBuyers
             // 
+            this.cb_ProductBuyers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cb_ProductBuyers.FormattingEnabled = true;
             this.cb_ProductBuyers.Location = new System.Drawing.Point(10, 63);
             this.cb_ProductBuyers.Name = "cb_ProductBuyers";
@@ -183,50 +238,15 @@
             this.lb_Products.FormattingEnabled = true;
             this.lb_Products.Location = new System.Drawing.Point(158, 18);
             this.lb_Products.Name = "lb_Products";
-            this.lb_Products.Size = new System.Drawing.Size(143, 264);
+            this.lb_Products.Size = new System.Drawing.Size(143, 212);
             this.lb_Products.TabIndex = 0;
-            // 
-            // btn_BuyAnimal
-            // 
-            this.btn_BuyAnimal.Location = new System.Drawing.Point(85, 243);
-            this.btn_BuyAnimal.Name = "btn_BuyAnimal";
-            this.btn_BuyAnimal.Size = new System.Drawing.Size(67, 39);
-            this.btn_BuyAnimal.TabIndex = 6;
-            this.btn_BuyAnimal.Text = "Buy";
-            this.btn_BuyAnimal.UseVisualStyleBackColor = true;
-            // 
-            // btn_BuyProduct
-            // 
-            this.btn_BuyProduct.Location = new System.Drawing.Point(85, 243);
-            this.btn_BuyProduct.Name = "btn_BuyProduct";
-            this.btn_BuyProduct.Size = new System.Drawing.Size(67, 39);
-            this.btn_BuyProduct.TabIndex = 7;
-            this.btn_BuyProduct.Text = "Buy";
-            this.btn_BuyProduct.UseVisualStyleBackColor = true;
-            // 
-            // btn_AddProduct
-            // 
-            this.btn_AddProduct.Location = new System.Drawing.Point(10, 243);
-            this.btn_AddProduct.Name = "btn_AddProduct";
-            this.btn_AddProduct.Size = new System.Drawing.Size(67, 39);
-            this.btn_AddProduct.TabIndex = 8;
-            this.btn_AddProduct.Text = "Add";
-            this.btn_AddProduct.UseVisualStyleBackColor = true;
-            // 
-            // btn_ReserveAnimal
-            // 
-            this.btn_ReserveAnimal.Location = new System.Drawing.Point(6, 243);
-            this.btn_ReserveAnimal.Name = "btn_ReserveAnimal";
-            this.btn_ReserveAnimal.Size = new System.Drawing.Size(67, 39);
-            this.btn_ReserveAnimal.TabIndex = 7;
-            this.btn_ReserveAnimal.Text = "Reserve";
-            this.btn_ReserveAnimal.UseVisualStyleBackColor = true;
+            this.lb_Products.SelectedIndexChanged += new System.EventHandler(this.lb_Products_SelectedIndexChanged);
             // 
             // WebShopForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(650, 313);
+            this.ClientSize = new System.Drawing.Size(650, 261);
             this.Controls.Add(this.gb_Products);
             this.Controls.Add(this.gb_animals);
             this.Name = "WebShopForm";
@@ -259,6 +279,7 @@
         private System.Windows.Forms.Button btn_AddProduct;
         private System.Windows.Forms.Button btn_BuyProduct;
         private System.Windows.Forms.Button btn_ReserveAnimal;
+        private System.Windows.Forms.Button btn_ReturnToAdminForm;
 
     }
 }
--- AnimalShelter/WebShopForm.cs
+++ AnimalShelter/WebShopForm.cs
@@ -7,23 +7,134 @@ namespace AnimalShelter
 {
     public partial class WebShopForm : Form
     {
-        private readonly Animal animal;
-
-        public WebShopForm(Animal Animal)
+        public WebShopForm()
         {
-            animal = Animal;
             InitializeComponent();
+            ShowData();
         }
 
-        private void btn_Reserve_Click(object sender, EventArgs e)
+        private void ShowData()
         {
-            if (tb_ReservorName.Text == "" || tb_ReservorName == null)
+            lb_Products.DataSource = Webshop.products;
+            lb_Animals.DataSource = Webshop.animals;
+            cb_AnimalBuyers.DataSource = Webshop.buyers;
+            cb_ProductBuyers.DataSource = Webshop.buyers;
+        }
+
+        private void btn_AddProduct_Click(object sender, EventArgs e)
+        {
+            ProductForm productForm = new ProductForm();
+            productForm.Show();
+        }
+
+        private void btn_BuyProduct_Click(object sender, EventArgs e)
+        {
+            Buyer buyer;
+            if (cb_ProductBuyers.SelectedItem != null)
             {
-                MessageBox.Show("Insert name please");
+                buyer = (Buyer) cb_ProductBuyers.SelectedItem;
             }
-            Reservor reservor = new Reservor(tb_ReservorName.Text, DateTime.Now);
-            animal.IsReserved = reservor.ToString();
+            else
+            {
+                buyer = new Buyer(tb_BuyerName.Text);
+                Webshop.buyers.Add(buyer);
+            }
+            Product product = (Product)lb_Products.SelectedItem;
+
+            MessageBox.Show(buyer.Name + ", you have bought a " + product.Name +
+                            "! We hope you enjoy your purchase.");
+            cb_ProductBuyers.SelectedIndex = -1;
+        }
+
+        private void lb_Products_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Product product = (Product) lb_Products.SelectedItem;
+            lbl_ShowProductPrice.Text = product.Price.ToString();
+        }
+
+        private void btn_ReserveAnimal_Click(object sender, EventArgs e)
+        {
+            Animal animal = (Animal) lb_Animals.SelectedItem;
+
+            //Making sure animal isn't already reserved
+            if (animal.IsReserved != null)
+            {
+                MessageBox.Show("Whoops! We're sorry to tell you, but " + animal.Name +
+                                " has already been taken by " + animal.IsReserved);
+            }
+
+            else
+            {
+                Reservor reservor;
+                if (cb_AnimalBuyers.SelectedItem != null)
+                {
+                    reservor = new Reservor(cb_AnimalBuyers.SelectedItem.ToString(), DateTime.Now);
+                }
+                else
+                {
+                    reservor = new Reservor(tb_ReservorName.Text, DateTime.Now);
+                    Webshop.buyers.Add(reservor);
+                }
+                
+                animal.IsReserved = reservor.Name;
+                MessageBox.Show("Congratulations " + reservor.Name + "! You have reserved " + animal.Name + "!");
+            }
+            cb_AnimalBuyers.SelectedIndex = -1;
+            cb_ProductBuyers.SelectedIndex = -1;
+        }
+
+        private void btn_BuyAnimal_Click(object sender, EventArgs e)
+        {
+            Animal animal = (Animal) lb_Animals.SelectedItem;
+
+            //Making sure animal isn't already reserved
+            if (animal.IsReserved != null)
+            {
+                MessageBox.Show("Whoops! We're sorry to tell you, but " + animal.Name +
+                                " has already been taken by " + animal.IsReserved);
+            }
+            else
+            {
+                Buyer buyer;
+                if (cb_AnimalBuyers.SelectedItem != null)
+                {
+                    buyer = (Buyer) cb_AnimalBuyers.SelectedItem;
+                }
+                else
+                {
+                    buyer = new Buyer(tb_ReservorName.Text);
+                }
+
+                Webshop.animals.Remove(animal);
+                MessageBox.Show("Congratulations " + buyer.Name + "! You have bought " + animal.Name +
+                                "! We hope you will have much joy with your new little buddy!");
+            }
+            cb_AnimalBuyers.SelectedIndex = -1;
+            cb_ProductBuyers.SelectedIndex = -1;
+        }
+
+        private void btn_ReturnToAdminForm_Click(object sender, EventArgs e)
+        {
             Close();
+        }
+
+        private void lb_Animals_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            decimal animalPrice;
+            if (lb_Animals.SelectedItem is Cat)
+            {
+                Cat cat = lb_Animals.SelectedItem as Cat;
+                animalPrice = 350 - (cat.BadHabit.Length*20);
+                cat.Price = animalPrice;
+            }
+            if (lb_Animals.SelectedItem is Dog)
+            {
+                Dog dog = lb_Animals.SelectedItem as Dog;
+                animalPrice = 500 - ((Webshop.dogs.Count - Webshop.dogs.IndexOf(dog) - 1) *50);
+                dog.Price = animalPrice;
+            }
+            Animal animal = (Animal)lb_Animals.SelectedItem;
+            lbl_ShowAnimalPrice.Text = animal.Price.ToString();
         }
     }
 }
