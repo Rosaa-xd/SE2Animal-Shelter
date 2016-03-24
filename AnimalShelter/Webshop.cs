ed54e859cbae2483f34633ef2fda244173926d37
Rosaa-xD <github@shadowphoenix.nl>
2016-03-25 00:22:53 +0100

    Added static class webshop to hold all bindinglists for the application

---
+++ AnimalShelter/Webshop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelter
+{
+    public static class Webshop
+    {
+        //BindingList to live update listboxes
+        public static BindingList<Animal> animals; 
+        public static BindingList<Product> products;
+        public static BindingList<Buyer> buyers;
+        public static List<Dog> dogs; 
+
+        static Webshop()
+        {
+            animals = new BindingList<Animal>();
+            products = new BindingList<Product>();
+            buyers = new BindingList<Buyer>();
+            dogs = new List<Dog>();
+        }
+    }
+}
